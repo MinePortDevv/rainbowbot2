b2ba9b244b061d438825d518c221cde387dde91c
Vuuuult <43483379+Vuuuult@users.noreply.github.com>
2019-01-07 09:54:20 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("1help | By hassan_games",{type: 'WATCHING'})
+   client.user.setActivity("Varz Games..",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
