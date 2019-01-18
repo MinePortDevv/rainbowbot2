826991d53239aa0b82ee2ea0cae85ffc8758917d
Vuuuult <43483379+Vuuuult@users.noreply.github.com>
2019-01-18 13:42:27 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,19 @@ client.on("ready", () => {
     client.guilds.get('474838434343813120').roles.find("name", "rainbow").setColor("RANDOM");
   };
   setInterval(lol, 1000);
+  
 });
- 
+client.on('message', message => {
+              var prefix = "=invite" ;
+            if (message.content.startsWith(prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)   
+.addField('https://discordapp.com/oauth2/authorize?client_id=532329689344114698&scope=bot&permissions=268435456')
+
+
+.setColor('RANDOM')
+  message.author.sendEmbed(embed);
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
